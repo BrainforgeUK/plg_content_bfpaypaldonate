2b612af369913292594fa1451d8652a5e1bf8cd5
Brainforge.UK <brainforge.uk@btinternet.com>
2022-08-27 08:10:07 +0100

    Code tidyup for use on Joomla 4 upgrade.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-This plugin allows you to insert a Paypal donate button into an article.
+This plugin allows you to insert a Paypal donate button into an article or module by using:
+``{bfpaypaldonate}``
--- bfpaypaldonate.php
+++ bfpaypaldonate.php
@@ -3,7 +3,7 @@
 * @package   Paypal Donate Button
 * @version   0.0.1
 * @author    http://www.brainforge.co.uk
-* @copyright Copyright (C) 2012 Jonathan Brain. All rights reserved.
+* @copyright Copyright (C) 2012-2022 Jonathan Brain. All rights reserved.
 * @license	 GNU/GPLv3 http://www.gnu.org/licenses/gpl-3.0.html
 */
 
@@ -13,51 +13,75 @@ defined('_JEXEC') or die;
 jimport('joomla.plugin.plugin');
 
 class plgContentBFPaypalDonate extends JPlugin{
-  private $accountinfo;  
-  private static $donateform = false;  
-  /**
-   *
-   */
-	public function __construct(&$subject, $config = array()) {
-	  parent::__construct($subject, $config);
-    $this->accountinfo = $this->params->def('accountinfo');
-  }
+	private $accountinfo;
+	private static $donateform = false;
 
-  /**
-   *
-   */
-	public function onContentPrepare($context, &$article, &$params, $limitstart){	
-    if (empty($this->accountinfo)) return;
+	/**
+	*/
+	public function __construct(&$subject, $config = array())
+	{
+		parent::__construct($subject, $config);
+		$this->accountinfo = $this->params->def('accountinfo');
+	}
 
-		$matches = array();
-		if (preg_match_all('/{(bfpaypaldonate)\s*(.*?)}/i', $article->text, $matches, PREG_SET_ORDER)) {
-  		foreach ($matches as $match){
-  		  $article->text = $this->insertDonateButton($match[0], $article->text, trim($match[2], '"'));
-  		} 		
-  	}
-  }
+	/**
+	*
+	*/
+	public function onContentPrepare($context, &$article, &$params, $limitstart)
+	{
+		if (empty($this->accountinfo)) return;
 
-  /**
-   *
-   */
-  private function insertDonateButton($match, $text, $comment=null) {
-    if (empty($this->accountinfo)) return;
-    
-    $text = str_replace($match, '<button class="donateClickButton" onclick="jQuery(\'#donateClickButton\').submit();return false;">' .
-                                $comment . '<br/>' .
-                                '<img src="https://www.paypalobjects.com/en_GB/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal — The safer, easier way to pay online."/></button>' .
-                                '<img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">', $text);
-    if (self::$donateform) {
-      return $text;
-    }
-    self::$donateform = true;
+			$matches = array();
+			if (preg_match_all('/{(bfpaypaldonate)\s*(.*?)}/i', $article->text, $matches, PREG_SET_ORDER))
+			{
+		    foreach ($matches as $match)
+			{
+				$article->text = $this->insertDonateButton($match[0], $article->text, trim($match[2], '"'));
+		    }
+		}
+	}
 
-    return $text . '<form id="donateClickButton" action="https://www.paypal.com/cgi-bin/webscr" method="post">
-<input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="encrypted" value="' . $this->accountinfo . '
-">
-</form>
-';
-  }
+	/*
+	*/
+	private function insertDonateButton($match, $text, $comment=null)
+	{
+		if (empty($this->accountinfo)) return;
+
+		ob_start();
+		?>
+		<button class="donateClickButton"
+		  onclick="document.getElementById('donateClickButton').submit();return false;" >
+			<?php echo empty($comment) ? '' : $comment . '<br/>'; ?>
+			<img src="https://www.paypalobjects.com/en_GB/i/btn/btn_donate_LG.gif"
+			     style="border:none;"
+			     name="submit"
+			     alt="PayPal ï¿½ The safer, easier way to pay online." />
+		</button>
+
+		<img alt=""
+		     style="border:none"
+		     src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1"/>
+		<?php
+		$html = ob_get_clean();
+
+		$html = str_replace($match, $html, $text);
+
+		if (self::$donateform)
+		{
+			return $html;
+		}
+		self::$donateform = true;
+
+		ob_start();
+		?>
+			<form id="donateClickButton"
+			      action="https://www.paypal.com/cgi-bin/webscr"
+			      method="post">
+				<input type="hidden" name="cmd" value="_s-xclick"/>
+				<input type="hidden" name="encrypted" value="<?php echo $this->accountinfo; ?>">
+			</form>
+		<?php
+		return $html . ob_get_clean();
+	}
 }
 ?>
